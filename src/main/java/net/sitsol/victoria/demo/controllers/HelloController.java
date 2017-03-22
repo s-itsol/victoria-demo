d75f1e5f9c6c81d29fd8c359bc8de41481e00af3
rei.shibano <shibano@s-itsol.net>
2017-03-23 02:18:28 +0900

    微調整

--- src/main/java/net/sitsol/victoria/demo/controllers/HelloController.java
+++ src/main/java/net/sitsol/victoria/demo/controllers/HelloController.java
@@ -1,9 +1,10 @@
 /**
  *
  */
-package net.sitsol.victoria.demo.controller;
+package net.sitsol.victoria.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,14 +12,14 @@ import net.sitsol.victoria.log4j.VctLogger;
 
 /**
  *
- * @author rei_shibano
+ * @author shibano
  */
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
 	@RequestMapping(value = "/world.do", method = RequestMethod.GET)
-	public String world() {
+	public String world(Model model) {
 
 		VctLogger.getLogger().info("HelloController-world.doメソッド実行開始");
 
