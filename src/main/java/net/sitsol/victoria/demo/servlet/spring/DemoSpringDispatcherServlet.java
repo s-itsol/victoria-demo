1ba28c5bba531016b7f11f4e4d3a7e3862190af0
rei.shibano <shibano@s-itsol.net>
2018-02-04 19:28:34 +0900

    今のところ何も処理は無いが、オーバーライド実装できる事をわかりやすくするため、空のオーバーライドだけ実装しておく。

--- src/main/java/net/sitsol/victoria/demo/servlet/spring/DemoSpringDispatcherServlet.java
+++ src/main/java/net/sitsol/victoria/demo/servlet/spring/DemoSpringDispatcherServlet.java
@@ -3,6 +3,10 @@
  */
 package net.sitsol.victoria.demo.servlet.spring;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sitsol.victoria.log4j.VctLogger;
 import net.sitsol.victoria.setvlet.spring.VctSpringDispatcherServlet;
 
 /**
@@ -13,5 +17,30 @@ import net.sitsol.victoria.setvlet.spring.VctSpringDispatcherServlet;
 public class DemoSpringDispatcherServlet extends VctSpringDispatcherServlet {
 
 	private static final long serialVersionUID = 5478210285799598883L;
+
+	/**
+	 * サービス実行
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
+	 */
+	@Override
+	protected void doService(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// 基底クラスのメソッド実行
+		super.doService(request, response);
+	}
+
+	/**
+	 * ディスパッチ実行
+	 * @param request HTTPサーブレットリクエスト
+	 * @param response HTTPサーブレットレスポンス
+	 */
+	@Override
+	protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		VctLogger.getLogger().info("★doDispatch");
+
+		// 基底クラスのメソッド実行
+		super.doDispatch(request, response);
+	}
 
 }
