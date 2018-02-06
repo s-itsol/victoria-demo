14192e93e3395b1d15897bfceaa472432805c6c4
rei.shibano <shibano@s-itsol.net>
2018-02-06 23:36:27 +0900

    メッセージソースの使用サンプル

--- src/main/java/net/sitsol/victoria/demo/ctrls/HelloCtrl.java
+++ src/main/java/net/sitsol/victoria/demo/ctrls/HelloCtrl.java
@@ -3,12 +3,13 @@
  */
 package net.sitsol.victoria.demo.ctrls;
 
+import net.sitsol.victoria.log4j.VctLogger;
+import net.sitsol.victoria.messages.VctMessage;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import net.sitsol.victoria.log4j.VctLogger;
 
 /**
  *
@@ -23,6 +24,9 @@ public class HelloCtrl {
 
 		VctLogger.getLogger().info("HelloController-world.doメソッド実行開始");
 
+		String msg = VctMessage.getMessage("errors.orver", "スイカ", "2つ");
+		VctLogger.getLogger().info(" -> メッセージ取得テスト：[" + msg + "]");
+		
 		// vmのファイルパス(拡張子なし)を返す
 		return "helloworld";
 	}
